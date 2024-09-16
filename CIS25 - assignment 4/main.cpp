0537e4a3edc6053595adfe10ddc66acb87be4ec5
maya-nachiappan <maya.n102304@gmail.com>
2024-09-15 18:23:16 -0700

    setting and printing left alignment for text and right alignment for numbers

--- CIS25 - assignment 4/main.cpp
+++ CIS25 - assignment 4/main.cpp
@@ -6,7 +6,18 @@
 
 #include <iostream>
 #include <iomanip>
+using namespace std;
+
 int main()
 {
+    // initializing column width
+    const int colWidth = 12;
+    cout << setw(colWidth) << left << "Course"
+    << setw(colWidth) << left << "Students" << endl;
+    cout << setw(colWidth) << left << "C++"
+    << setw(colWidth) << right << "100" << endl;
+    cout << setw(colWidth) << left << "JavaScript"
+    << setw(colWidth) << right << "50" << endl;
     
+    return 0;
 }
